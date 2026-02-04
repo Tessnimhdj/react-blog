eae46b8b8c4bf6bf25451000c1ea3ad1468fecbb
Tessnimhdj <thadjredjem@gmail.com>
2026-02-04 22:39:06 +0100

    Initial React project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Topbar from "./Components/Topbar/Topbar";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Pages/Home/Home";
+function App() {
+  return (
+    <>
+      <Topbar />
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
